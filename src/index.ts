2725e85a887fcd9f97c036350e58b83d118f5bf2
aoyi <aoyi@qq.com>
2026-07-01 12:16:52 +0800

    refactor: export Loading component in global index (Task 4)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 // yuan-ui 开源组件库最终入口
 import './styles/theme.css'
 
-export * from './components/ChainOfThought'
 export * from './components/AgentTrace'
 export * from './components/AgentTrace/context'
 export * from './components/AgentTrace/types'
+export * from './components/AgentTraceV2'
+export * from './components/Loading'
+
